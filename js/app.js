c8033b0ba6f88a83fbe52913b4e7d4c4077a7cf8
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-04-27 11:14:24 +0200

    Remove demo cube

--- js/app.js
+++ js/app.js
@@ -12,24 +12,16 @@ export function init() {
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
   camera.position.z = 1000;
 
-  geometry = new THREE.BoxGeometry( 200, 200, 200 );
-  material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-
-  mesh = new THREE.Mesh( geometry, material );
-  scene.add( mesh );
-
   renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
 
   document.body.appendChild( renderer.domElement );
+
+  animate();
 }
 
 function animate() {
-
   requestAnimationFrame( animate );
-
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
 
   renderer.render( scene, camera );
 }
